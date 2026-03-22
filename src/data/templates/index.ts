ed039f5867e2db12bdf74f297ead7d990c2d5d0d
msaidbilgehan <msaidbilgehan@gmail.com>
2026-03-22 12:51:03 +0300

    All templates separated into importable json files

--- src/data/templates/index.ts
+++ src/data/templates/index.ts
@@ -1,5 +1,22 @@
 import type { Template } from "@/shared/types/entities";
-import templatesExport from "../../../Exports/templates.json";
 
-/** Bundled templates loaded from Exports/templates.json */
-export const BUNDLED_TEMPLATES: Template[] = templatesExport.templates as unknown as Template[];
+import cookieBannerDismisser from "../../../Templates/cookie-banner-dismisser.json";
+import videoNavigator from "../../../Templates/video-navigator.json";
+import elementHider from "../../../Templates/element-hider.json";
+import darkModeInjector from "../../../Templates/dark-mode-injector.json";
+import autoClicker from "../../../Templates/auto-clicker.json";
+
+interface TemplateFile {
+  templates: unknown[];
+}
+
+/** Bundled templates loaded from Templates/*.json */
+export const BUNDLED_TEMPLATES: Template[] = (
+  [
+    cookieBannerDismisser,
+    videoNavigator,
+    elementHider,
+    darkModeInjector,
+    autoClicker,
+  ] as TemplateFile[]
+).flatMap((file) => file.templates) as Template[];
